71a35e839733b059fb7f320e517fc73ba23acb0d
Aquil Sheikh <aquil.she7kh@gmail.com>
2025-08-15 18:37:00 -0400

    Used std::map to deal with parameters to diff eqs on the cpp side

--- src/cpp/Numerical.h
+++ src/cpp/Numerical.h
@@ -2,10 +2,14 @@
 #define NUMERICAL_H
 #include <vector>
 #include <functional>
+#include <map>
+#include <string>
+#include "StateVector.h"
 namespace numerical {
     using StateVector = std::vector<double>;
-    using DifferentialEquationFunction = std::function<std::vector<double>(double, std::vector<double>)>;
     using Time = double;
+    using Parameters = std::map<std::string, double>;
+    using DifferentialEquationFunction = std::function<StateVector(Time, StateVector, Parameters)>;
     std::vector<double> rk4(DifferentialEquationFunction &diff_eq, // takes t, y as input and outputs a y based off of it
            Time &t, 
            StateVector &y);
@@ -15,6 +19,7 @@ namespace numerical {
         std::vector<Time> &t_span,
         StateVector &y0,
         std::vector<Time> &t_eval,
+        Parameters params
     );
 
 }
--- src/cpp/StateVector.h
+++ src/cpp/StateVector.h
@@ -1,9 +1,7 @@
 #ifndef STATE_VECTOR_H
 #define STATE_VECTOR_H
 
-#include <csignal>
 #include <cstdio>
-#include <iterator>
 #include <vector>
 #include <algorithm>
 #include <functional>
@@ -12,27 +10,56 @@ class StateVector // wrapper class for std::vector with + operation
 {
     public:
       StateVector(std::vector<double> value) : m_state(value) {};
+
+      // Rule of Five
+      StateVector(StateVector &other) {
+        m_state = other.m_state;
+      }
+
+      StateVector(StateVector&& other) {
+        m_state = std::move(other.m_state);
+      }
+
+      StateVector& operator=(const StateVector &other) {
+        m_state = other.m_state;
+        return *this;
+      }
+
+      StateVector& operator=(const StateVector &&other) {
+        m_state = std::move(other.m_state);
+        return *this;
+      }
+
+      ~StateVector() {};
+
+      // Adding state vectors
       StateVector operator+(StateVector &other) const {
-        if (m_state.size() != other.size()) {
+        if (m_state.size() != other.m_state.size()) {
             perror("StateVector: INVALID ADDITION, DIMENSION OF OPERANDS DO NOT MATCH");
             exit(1);
         }
-        std::vector<double> result(other.size());
+        std::vector<double> result(other.m_state.size());
         std::transform(m_state.begin(), 
                        m_state.end(),
-                       other.begin(), 
+                       other.m_state.begin(), 
                        result.begin(),
                        std::plus<double>());
         return StateVector(result);    
       };
-      StateVector& operator=(const StateVector &other) {
-        
+
+      // Scalar multiplication
+      StateVector operator*(double &scalar) {
+        std::vector<double> result(m_state.size());
+        std::transform(m_state.begin(), 
+                       m_state.end(), 
+                       result.begin(), 
+                       [scalar](double y) {return scalar * y;});
+        return StateVector(result);
       }
-      std::vector<double> getState() { return m_state;} // breaks encapsulation :/
+
+      std::vector<double> getState() const { return m_state;} // breaks encapsulation :/
     private:
       std::vector<double> m_state;
-      int size() { return m_state.size();}
-      std::vector<double>::iterator begin() {return m_state.begin();}
 };
 
 #endif
